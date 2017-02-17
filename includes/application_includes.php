42945cf715c960c0e354fb48fc8d36b2c54fe667
Armenhammer <caleb46176@gmail.com>
2017-02-16 20:06:07 -0500

    Deliverable 3

--- includes/application_includes.php
+++ includes/application_includes.php
@@ -5,6 +5,7 @@
  * Date: 2/9/2017
  * Time: 7:16 PM
  */
-require_once($_SERVER['DOCUMENT_ROOT'].'/incldues/config.php');
-
+// Include the basic configuration elements
+require_once($_SERVER['DOCUMENT_ROOT'].'/../includes/config.php');
+// Include the database connection and query class
 require_once(FS_INCLUDES . 'Database.php');
